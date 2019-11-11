4cb4b3d8b34d4d254a16a8e5f7fe1519efdff4a4
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-11 16:13:55 -0500

    changes

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,40 @@
-require_relative 'config/environment'
+require 'pry'
+require_relative '../config/environment'
 
+# api.openweathermap.org/data/2.5/weather?q={city name}
+
+# unsure of commas in has_many realtionships classes
+class Search
+def run
+    welcome
+    city_name = get_user_input
+    response = call_api(city_name)
+    binding.pry
+end
+
+def welcome
+    puts "welcome"
+end
+
+def get_user_input
+    puts "Please enter a city:"
+    gets.chomp
+end
+
+def call_api(city_name)
+    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&APPID=827a998e41cda6984e61e05673fb503b"
+    response = RestClient.get(url)
+    parse_response(response)
+end
+
+def parse_response(response)
+    JSON.parse(response)
+end
+
+
+#kthx
+end
+
+search = Search.new
+search.run
+# binding.pry
